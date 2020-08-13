02258826b63189286820eab0b1c6c5c718ee336c
Daniel Esteban Nombela <dani@gatunes.com>
2020-08-14 00:56:52 +0200

    another one

--- client/src/components/welcome.svelte
+++ client/src/components/welcome.svelte
@@ -9,7 +9,7 @@
       This is a beta test of the cryptoart tool and gallery I'm creating.
     </p>
     <p>
-      It features a <a href="#/creator">voxels editor</a> to create small dioramas/destinations which then you can mint into immutable NFT Tokens and trade them to make some money.
+      It features a <a href="#/creator">voxel editor</a> to create small dioramas/destinations which then you can mint into immutable NFT Tokens and trade them to make some money.
     </p>
     <p>
       When minting the NFT token, a cryptographic hash that uniquely indentifies the content gets stored within it.
